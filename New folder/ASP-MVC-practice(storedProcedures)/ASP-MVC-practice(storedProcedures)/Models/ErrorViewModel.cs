5052304d5b8980bb08d969b1d07075d1985d1a9e
unknown <wawerufelix60@gmail.com>
2020-07-20 13:48:35 +0300

    Stored Procedures ASP app

---
+++ New folder/ASP-MVC-practice(storedProcedures)/ASP-MVC-practice(storedProcedures)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_MVC_practice_storedProcedures_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
